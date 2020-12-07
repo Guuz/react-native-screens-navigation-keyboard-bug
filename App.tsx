dc74e66f77d35465831e9bed5eaeb2bc2856f8b0
Guus Goossens <guus.goossens@gmail.com>
2020-12-07 11:40:33 +0100

    Add textinput and header title.

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {Text, View} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import {StackNavigationProp} from '@react-navigation/stack';
+import {TextInput} from 'react-native-gesture-handler';
 
 type RootStackParamList = {
   Home: undefined;
@@ -25,9 +26,22 @@ type Props = {
 enableScreens();
 
 function HomeScreen({navigation}: Props) {
+  const [value, setValue] = React.useState('');
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({title: value});
+  }, [value, navigation]);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
+    <View style={{flex: 1}}>
+      <Text>Spacer to push input down</Text>
+      <View style={{backgroundColor: 'lightblue', height: 600, margin: 24}} />
+      <TextInput
+        value={value}
+        onChangeText={setValue}
+        placeholder="Type something..."
+        style={{borderColor: 'black', borderWidth: 1, width: 200, fontSize: 24}}
+      />
     </View>
   );
 }
